6bc17a8f046edbddb4c4bec005d375d025ae6d2d
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-20 00:22:22 +0400

    finish task 6

---
+++ MyPlayground.playground/Pages/task5.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+//5. შექმენით ფუნქცია `calculateTax`, რომელსაც აქვს ორი პარამეტრი: Double ტიპის `price` და Double ტიპის `taxRate` (პროცენტებში). ფუნქციამ უნდა დააბრუნოს გადასახადის თანხა. გამოიძახეთ ეს ფუნქცია სხვადასხვა ფასისთვის და დაბეჭდეთ შედეგები.
+
+import Foundation
+
+func calculateTax(price: Double, taxRate: Double) -> Double {
+    
+    var tax = price * taxRate / 100
+    
+    return tax
+}
+
+calculateTax(price: 10.2, taxRate: 18)
+calculateTax(price: 1250, taxRate: 20)
+calculateTax(price: 56.9, taxRate: 13)
+
+//: [Next](@next)
